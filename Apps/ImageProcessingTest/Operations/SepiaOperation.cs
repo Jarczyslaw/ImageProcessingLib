4f085f3441dcae04da2cd8a49657333af3702600
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2018-05-06 20:13:20 +0200

    Sepia was added

---
+++ Apps/ImageProcessingTest/Operations/SepiaOperation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageProcessingLib;
+using ImageProcessingLib.ImageProcessing;
+using ImageProcessingLib.GDI;
+
+namespace ImageProcessingTest.Operations
+{
+    public class SepiaOperation : OperationBase
+    {
+        public override void AddImages(Dictionary<string, GDImage32> images, Image<Pixel32> originalImage)
+        {
+            var levels = new int[] { 10, 20, 30, 40, 80 };
+            foreach(var level in levels)
+            {
+                var image = originalImage.Copy();
+                image.Sepia(level);
+                images.Add("Sepia-" + level, new GDImage32(image));
+            }
+        }
+    }
+}
---
+++ ImageProcessingLib/ImageProcessing/SepiaExtension.cs
@@ -0,0 +1,28 @@
+﻿using ImageProcessingLib.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageProcessingLib.ImageProcessing
+{
+    public static class SepiaExtension
+    {
+        public static Image<Pixel32> Sepia(this Image<Pixel32> image, int level)
+        {
+            image.ForEach((x, y) =>
+            {
+                var pixel = image.Get(x, y);
+                image.Set(x, y, Sepia(pixel, level));
+            });
+            return image;
+        }
+
+        public static Pixel32 Sepia(Pixel32 pixel, int level)
+        {
+            var r = MathUtils.ByteClamp(pixel.R + 2 * level);
+            var g = MathUtils.ByteClamp(pixel.G + level);
+            var b = pixel.B;
+            return new Pixel32(pixel.A, r, g, b);
+        }
+    }
+}
