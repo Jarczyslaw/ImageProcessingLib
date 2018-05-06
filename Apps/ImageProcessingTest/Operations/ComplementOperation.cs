227a253afbd3955133b3e0839921f6298eae18f2
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2018-05-06 14:40:59 +0200

    ComplementExtension was added

---
+++ Apps/ImageProcessingTest/Operations/ComplementOperation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageProcessingLib;
+using ImageProcessingLib.GDI;
+
+namespace ImageProcessingTest.Operations
+{
+    public class ComplementOperation : OperationBase
+    {
+        public override void AddImages(Dictionary<string, GDImage32> images, Image<Pixel32> originalImage)
+        {
+            var originalWidth = originalImage.Width;
+            var originalHeight = originalImage.Height;
+            var newSizes = new Tuple<int, int>[]
+            {
+                Tuple.Create(originalWidth, originalHeight),
+                Tuple.Create(originalWidth + 50, originalHeight + 50),
+                Tuple.Create(originalWidth + 23, originalHeight + 32)
+            };
+
+            var image = originalImage.Copy();
+            image.Complement(originalWidth + 32, originalHeight + 32, Pixel32.Red);
+            images.Add("RedComplement", new GDImage32(image));
+            foreach (var size in newSizes)
+            {
+                image = originalImage.Copy();
+                image.Complement(size.Item1, size.Item2);
+                images.Add(string.Format("BlankComplement{0}x{1}", size.Item1, size.Item2), new GDImage32(image));
+            }
+        }
+    }
+}
---
+++ ImageProcessingLib/Core/ImageExtensions/ComplementExtension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageProcessingLib
+{
+    public static class ComplementExtension
+    {
+        public static Image<TPixelType> Complement<TPixelType>(this Image<TPixelType> image, int width, int height)
+            where TPixelType : struct, IPixel<TPixelType>
+        {
+            return image.Complement(width, height, new TPixelType().Blank);
+        }
+
+        public static Image<TPixelType> Complement<TPixelType>(this Image<TPixelType> image, int width, int height, TPixelType fillColor)
+            where TPixelType : struct, IPixel<TPixelType>
+        {
+            if (image.Width > width || image.Height > height)
+                throw new ArgumentException("Complemented images should be larger than original one");
+
+            int startX = (width - image.Width) / 2;
+            int startY = (height - image.Height) / 2;
+            var originalImage = image.Copy();
+            image.InitializeNew(width, height);
+            image.Clear(fillColor);
+            originalImage.ForEach((x, y) =>
+            {
+                var pixel = originalImage.Get(x, y);
+                image.Set(startX + x, startY + y, pixel);
+            });
+            return image;
+        }
+    }
+}
