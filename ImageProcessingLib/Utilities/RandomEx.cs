ceb4d249bd455f2df3b64e5ee4b10304ddf5c257
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2018-06-06 18:10:25 +0200

    added GaussianValuesSource to RandomEx

--- ImageProcessingLib/Utilities/RandomEx.cs
+++ ImageProcessingLib/Utilities/RandomEx.cs
@@ -8,9 +8,47 @@ namespace ImageProcessingLib.Utilities
 {
     public class RandomEx : Random
     {
+        public class GaussianValuesSource
+        {
+            private double? nextValue = null;
+            private Random random;
+
+            public GaussianValuesSource(Random random)
+            {
+                this.random = random;
+            }
+
+            public double Next(double mean, double stdDev)
+            {
+                double result;
+                if (!nextValue.HasValue)
+                {
+                    double u1 = 1d - random.NextDouble();
+                    double u2 = 1d - random.NextDouble();
+                    double R = Math.Sqrt(-2d * Math.Log(u1));
+                    double theta = 2d * Math.PI * u2;
+                    double z0 = R * Math.Cos(theta);
+                    double z1 = R * Math.Sin(theta);
+
+                    nextValue = z1;
+                    result = mean + z0 * stdDev;
+                }
+                else
+                {
+                    result = mean + nextValue.Value * stdDev;
+                    nextValue = null;
+                }
+                return result;
+            }
+        }
+
+        private GaussianValuesSource gaussianSource;
+
         public RandomEx() :
             base((int)(new DateTimeOffset(DateTime.Now).ToUnixTimeMilliseconds()))
-        { }
+        {
+            gaussianSource = new GaussianValuesSource(this);
+        }
 
         public void RandomImagePoint(ImageBase image, out int x, out int y)
         {
@@ -30,17 +68,12 @@ namespace ImageProcessingLib.Utilities
 
         public double NextGaussian(double stdDev)
         {
-            return NextGaussian(0d, stdDev);
+            return gaussianSource.Next(0d, stdDev);
         }
 
         public double NextGaussian(double mean, double stdDev)
         {
-            double u1 = 1d - NextDouble();
-            double u2 = 1d - NextDouble();
-            double R = Math.Sqrt(-2d * Math.Log(u1));
-            double theta = 2d * Math.PI * u2;
-            double z0 = R * Math.Cos(theta);
-            return mean + z0 * stdDev;
+            return gaussianSource.Next(mean, stdDev);
         }
     }
 }
