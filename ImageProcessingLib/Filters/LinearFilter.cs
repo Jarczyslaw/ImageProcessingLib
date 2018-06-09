3e44b0d38ccda28af862fee8d7ce839ed1b7d1ff
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2018-06-09 13:52:12 +0200

    small improvement in LinerFilter

--- ImageProcessingLib/Filters/LinearFilter.cs
+++ ImageProcessingLib/Filters/LinearFilter.cs
@@ -10,7 +10,8 @@ namespace ImageProcessingLib
         public int Range { get; private set; }
         public int Size { get; private set; }
         public int KernelLength { get; private set; }
-        public double[] Kernel { get; private set; }
+        public double Multiplier { get; private set; }
+        public int[] Kernel { get; private set; }
 
         public LinearFilter(double multiplier, int[,] kernel)
         {
@@ -18,30 +19,23 @@ namespace ImageProcessingLib
 
             Size = kernel.GetLength(0);
             Range = Size / 2;
-            KernelFlattening(multiplier, kernel);
+            Kernel = ArrayUtils.Flatten(kernel);
             KernelLength = Kernel.Length;
+            Multiplier = multiplier;
         }
 
         public byte Apply(byte[] neighbourhood)
         {
-            var result = 0d;
+            var result = 0;
             for (int i = 0; i < KernelLength; i++)
                 result += Kernel[i] * neighbourhood[i];
-            return MathUtils.RoundToByte(result);
+            return MathUtils.RoundToByte(result * Multiplier);
         }
 
         private void Validate(int[,] kernel)
         {
             if (!ArrayUtils.IsFilterMask(kernel))
                 throw new ArgumentException("Filter mask must be square, with odd number of rows and columns. Filter mask must be higher or equal than 3");
-        }
-
-        private void KernelFlattening(double multiplier, int[,] kernel)
-        {
-            var flat = ArrayUtils.Flatten(kernel);
-            Kernel = new double[flat.Length];
-            for (int i = 0; i < flat.Length; i++)
-                Kernel[i] = multiplier * flat[i];
         }
     }
 }
