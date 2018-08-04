77b6d825e02cde50ad3681c8d88b9f4fd0001c8e
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2018-08-04 21:15:23 +0200

    CharcoalSketch was added

---
+++ Apps/ImageProcessingLibExamples/Examples/CharcoalSketch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageProcessingLib;
+using ImageProcessingLib.Wrappers.WF;
+
+namespace ImageProcessingLibExamples.Examples
+{
+    public class CharcoalSketch : ExampleBase
+    {
+        public override void AddExampleImages(Dictionary<string, ImageWrapper> images, Image<Pixel32> originalImage)
+        {
+            var image = originalImage.Copy();
+            image.CharcoalSketch();
+            images.Add("CharcoalSketch", new ImageWrapper(image));
+        }
+    }
+}
---
+++ ImageProcessingLib/ImageProcessing/Effects/CharcoalSketchExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageProcessingLib
+{
+    public static class CharcoalSketchExtension
+    {
+        public static Image<Pixel32> CharcoalSketch(this Image<Pixel32> image, byte inversion = 127)
+        {
+            image.ApplyFilter(new SobelFilter())
+                .Grayscale()
+                .Inversion(inversion)
+                .ApplyFilter(new MedianFilter3());
+            return image;
+        }
+    }
+}
