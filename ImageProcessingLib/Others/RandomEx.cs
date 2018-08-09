c4480480e1ce475a24b14fac7be274d5e2207d0b
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2018-08-09 22:06:32 +0200

    dev

--- ImageProcessingLib/Others/RandomEx.cs
+++ ImageProcessingLib/Others/RandomEx.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ImageProcessingLib.Utilities
+namespace ImageProcessingLib
 {
     public class RandomEx : Random
     {
