a12c9e4e75913a60b8888eeb488cea4d728272df
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2018-08-16 19:26:27 +0200

    Skeletonization is now working

---
+++ Apps/ImageProcessingLibToFIPComparison/Comparisons/Skeletonization.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FIP;
+using ImageProcessingLib;
+
+namespace ImageProcessingLibToFIPComparison.Comparisons
+{
+    public class Skeletonization : IComparison
+    {
+        public Bitmap GetFIPResults(FIP.FIP fip, Bitmap originalImage)
+        {
+            return fip.Skeletonization(originalImage);
+        }
+
+        public Image<Pixel32> GetIPLResult(Image<Pixel32> originalImage)
+        {
+            var input = originalImage.CopyAs(p => p.ToPixel1());
+            input.Skeletonization();
+            return input.CopyAs(p => p.ToPixel32());
+        }
+    }
+}
--- ImageProcessingLib/ImageProcessing/MorphologyFiltration/SkeletonizationExtension.cs
+++ ImageProcessingLib/ImageProcessing/MorphologyFiltration/SkeletonizationExtension.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace ImageProcessingLib
 {
@@ -12,19 +10,14 @@ namespace ImageProcessingLib
 
             var width = image.Width;
             var height = image.Height;
-            var table = new int[width, height];
-            for (int i = 0; i < width; i++)
+
+            var data = GetImageData(image);
+            var skeletonizationTable = SkeletonizationAlgorithm(data, width, height);
+            var skeleton = ExtractSkeleton(skeletonizationTable, width, height);
+            return image.ForEach((x, y) =>
             {
-                for (int j = 0; j < height; j++)
-                {
-                    if (image.Get(i, j).Value)
-                        table[i, j] = 1;
-                }
-            }
-
-            SkeletonizationRecursive(table, width, height, 1);
-
-            return image;
+                image.Set(x, y, new Pixel1(skeleton[x, y]));
+            });
         }
 
         private static void Validate(Image<Pixel1> image)
@@ -33,22 +26,63 @@ namespace ImageProcessingLib
                 throw new ArgumentException("Image for skeletonization has to be larger than 3x3");
         }
 
-        private static void SkeletonizationRecursive(int[,] table, int width, int height, int skeleValue)
+        private static int[,] GetImageData(Image<Pixel1> image)
         {
-            var nextIteration = false;
+            var width = image.Width;
+            var height = image.Height;
+            var data = new int[width, height];
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    if (image.Get(i, j).Value)
+                        data[i, j] = 1;
+                }
+            }
+            return data;
+        }
+
+        private static int[,] SkeletonizationAlgorithm(int[,] table, int width, int height)
+        {
+            bool nextIteration;
+            int skeleValue = 0;
+            do
+            {
+                nextIteration = false;
+                skeleValue++;
+
+                var newTable = (int[,])table.Clone();
+                for (int i = 1; i < width - 1; i++)
+                {
+                    for (int j = 1; j < height - 1; j++)
+                    {
+                        if (table[i, j] == skeleValue)
+                        {
+                            newTable[i, j] = Math.Min(Math.Min(table[i - 1, j], table[i + 1, j]), Math.Min(table[i, j - 1], table[i, j + 1])) + 1;
+                            nextIteration = true;
+                        }
+                    }
+                }
+                table = newTable;
+            }
+            while (nextIteration);
+            return table;
+        }
+
+        private static bool[,] ExtractSkeleton(int[,] table, int width, int height)
+        {
+            var result = new bool[width, height];
             for (int i = 1; i < width - 1; i++)
             {
                 for (int j = 1; j < height - 1; j++)
                 {
-                    if (table[i, j] == skeleValue)
-                    {
-                        table[i, j] = Math.Min(Math.Min(table[i - 1, j], table[i + 1, j]), Math.Min(table[i, j - 1], table[i, j + 1])) + 1;
-                        nextIteration = true;
-                    }
+                    var current = table[i, j];
+                    var max = Math.Max(Math.Max(table[i - 1, j], table[i + 1, j]), Math.Max(table[i, j - 1], table[i, j + 1]));
+                    if (current == max && current != 0)
+                        result[i, j] = true;
                 }
             }
-            if (nextIteration)
-                SkeletonizationRecursive(table, width, height, skeleValue + 1);
+            return result;
         }
     }
 }
